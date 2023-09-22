1a021f935dfe585b5153de352cb177ee81fa24b4
thiago_panetta <thiagopanetta19@gmail.com>
2023-09-21 21:02:35 -0300

    WEB sobre colores

---
+++ index.js
@@ -0,0 +1,46 @@
+/*funcionamiento de los botones*/
+var btn1 = document.getElementById("yellow")
+    btn1.addEventListener("click", actYellow)
+
+    function actYellow() {
+        alert("su color es amarillo")
+    }
+
+var btn2 = document.getElementById("red")
+    btn2.addEventListener("click", actRed)
+
+    function actRed() {
+        alert("su color es rojo")
+    }
+
+var btn3 = document.getElementById("orange")
+    btn3.addEventListener("click", actOrange)
+    
+    function actOrange() {
+        alert("su color es naranja")
+    }
+
+var btn4 = document.getElementById("blue")
+    btn4.addEventListener("click", actBlue)
+
+    function actBlue() {
+        alert("su color es azul")
+    }
+
+var btn5 = document.getElementById("green")
+    btn5.addEventListener("click", actGreen)
+
+    function actGreen() {
+        alert("su color es verde")
+    }
+
+var btn6 = document.getElementById("Purple")
+    btn6.addEventListener("click", actPurple)
+
+    function actPurple() {
+        alert("su color es morado")
+    }
+
+
+
+
